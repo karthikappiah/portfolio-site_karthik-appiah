b07b0ad82bc15c3e35bbf53df72662708db3b326
Karthik Appiah <karthik@appiah.company>
2025-05-29 22:29:53 -0700

    chore: added profile picture

--- src/components/page/headline.tsx
+++ src/components/page/headline.tsx
@@ -4,20 +4,20 @@ import {
   AvatarFallback,
 } from "@/components/shadcn/avatar";
 
-export const Headline = ({
-  src,
-  alt,
-  fallback,
-}: {
-  src: string;
-  alt: string;
-  fallback: string;
-}) => {
+export const Headline = ({ src, alt }: { src: string; alt: string }) => {
+  const getPacificTime = () => {
+    return new Date().toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZone: "America/Los_Angeles",
+      hour12: true,
+    });
+  };
   return (
     <div className="flex items-center space-x-4">
-      <Avatar className="size-11">
+      <Avatar className="size-20">
         <AvatarImage src={src} alt={alt} />
-        <AvatarFallback>{fallback}</AvatarFallback>
+        <AvatarFallback>{getPacificTime()}</AvatarFallback>
       </Avatar>
       <div>
         <h1 className="text-lg leading-relaxed font-medium">
